dd2b73b06d18333a6a9e5424db8da88a56f2ecae
srinidhi <srinidhigs1997@gmail.com>
2022-01-31 15:37:16 +0530

    2

--- r1.rb
+++ r1.rb
@@ -1,3 +1,11 @@
 #Write Ruby program to get ruby version with patch number.
 puts "Ruby version:"+RUBY_VERSION
 puts "Ruby Patch LEVEL:"+RUBY_PATCHLEVEL.to_s
+
+
+# Write a Ruby program to display the current date and time.
+require 'date'
+current_time = DateTime.now
+ctc = current_time.strftime "%d/%m/%Y/ %H:%M"
+
+puts "current_time " +ctc
