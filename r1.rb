9d3ea1f44e3307816f38ee25a00928c5d5c886f7
srinidhi <srinidhigs1997@gmail.com>
2022-01-31 15:43:27 +0530

    3

--- r1.rb
+++ r1.rb
@@ -1,11 +1,23 @@
-#Write Ruby program to get ruby version with patch number.
-puts "Ruby version:"+RUBY_VERSION
-puts "Ruby Patch LEVEL:"+RUBY_PATCHLEVEL.to_s
+#1.Write Ruby program to get ruby version with patch number.
+# puts "Ruby version:"+RUBY_VERSION
+# puts "Ruby Patch LEVEL:"+RUBY_PATCHLEVEL.to_s
 
 
-# Write a Ruby program to display the current date and time.
-require 'date'
-current_time = DateTime.now
-ctc = current_time.strftime "%d/%m/%Y/ %H:%M"
+# 2.Write a Ruby program to display the current date and time.
+# require 'date'
+# current_time = DateTime.now
+# ctc = current_time.strftime "%d/%m/%Y/ %H:%M"
 
-puts "current_time " +ctc
+# puts "current_time " +ctc
+
+# 3.Write a Ruby program to create a new string which is n copies of a given string where n is a non-negative integer.
+
+    def multiple(str,n)
+        return str*n
+    end
+
+    puts multiple('b',1)
+    puts multiple('b',4)
+
+
+
